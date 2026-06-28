2fdfffb00ea2e65ea5dc87d1050897bf79256a5a
sonallycecilia <sonallycecilia11@gmail.com>
2026-06-28 14:48:44 -0300

    reorganiza estrutura: move task-service para services/ e remove backend monolitico duplicado

--- CRUD de Tarefa/hello/src/main/java/com/example/justDoIt/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {}
